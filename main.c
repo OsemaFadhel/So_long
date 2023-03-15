f8d126b39483b318a5da5059d2e69a65f93b9035
Osema Fadhel <123255472+OsemaFadhel@users.noreply.github.com>
2023-03-15 19:21:23 +0100

    so_long

--- main.c
+++ main.c
@@ -1,34 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/14 11:15:20 by ofadhel           #+#    #+#             */
-/*   Updated: 2023/03/15 19:19:42 by ofadhel          ###   ########.fr       */
+/*   Created: 2023/03/14 13:52:10 by ofadhel           #+#    #+#             */
+/*   Updated: 2023/03/15 19:18:40 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef  SO_LONG_H
-# define SO_LONG_H
-
-# include <mlx.h>
-# include "libft/libft.h"
-# include <unistd.h>
-
-# define TILE_WIDTH 64
+#include <mlx.h>
 
 typedef struct	s_vars {
 	void	*mlx;
 	void	*win;
 }				t_vars;
 
-typedef struct s_game
+int	close(int keycode, t_vars *vars)
 {
-    ... /* all sprites */
-}               t_game
+	mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
+}
 
-int close(int keycode, t_vars *vars);
+int	main(void)
+{
+	t_vars	vars;
 
-#endif
+	/* checks */
+
+	drawmap...;
+	mlx_hook(vars.win, key, &game);
+	mlx_key_hook(vars.win, movement, &game);
+	mlx_loop(vars.mlx);
+}
--- src/close.c
+++ src/close.c
@@ -1,34 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   close.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/14 11:15:20 by ofadhel           #+#    #+#             */
-/*   Updated: 2023/03/15 19:19:42 by ofadhel          ###   ########.fr       */
+/*   Created: 2023/03/14 17:06:52 by ofadhel           #+#    #+#             */
+/*   Updated: 2023/03/14 18:00:05 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef  SO_LONG_H
-# define SO_LONG_H
+#include "../solong/so_long.h"
 
-# include <mlx.h>
-# include "libft/libft.h"
-# include <unistd.h>
-
-# define TILE_WIDTH 64
-
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
-
-typedef struct s_game
+int close(int keycode, t_vars *vars)
 {
-    ... /* all sprites */
-}               t_game
-
-int close(int keycode, t_vars *vars);
-
-#endif
+    mlx_destroy_window(vars->mlx, vars->win);
+    return (0);
+}
